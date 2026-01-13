22deecea4c3cbc379fccbcb328c097c5451beb20
fredrck rangara <ybnfredae@gmail.com>
2026-01-13 19:50:30 +0300

    finished editing app.route /stay sessions

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, make_response
+from models import db, StaySession, Pet, User
 from flask_migrate import Migrate
 from flask_cors import CORS
 from models import db  # Importing the db from your models.py
@@ -27,6 +28,13 @@ def create_stay_session():
             daily_rate=float(data.get('daily_rate')),
             special_instructions=data.get('special_instructions')
         )
+
+        db.session.add(new_session)
+        db.session.commit()
+        
+        return make_response(new_session.to_dict(), 201)
+    except Exception as e:
+        return make_response({"errors": [str(e)]}, 400)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
