f1c73150de1ada0bf9c6a33bc2b18ebc6f59378d
fredrck rangara <ybnfredae@gmail.com>
2026-01-13 20:04:43 +0300

    declared functions

--- client/src/pages/BookingForm.js
+++ client/src/pages/BookingForm.js
@@ -1,3 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
+
+function BookingForm() {
+    const [pets, setPets] = useState([]);
+    const [sitters, setSitters] = useState([]);
+
+    // Load dropdown data on component mount
+    useEffect(() => {
+        fetch('http://localhost:5555/pets').then(r => r.json()).then(setPets);
+        fetch('http://localhost:5555/users').then(r => r.json()).then(setSitters);
+    }, []);
